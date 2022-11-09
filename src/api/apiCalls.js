7457a50595c592343621473ff0adbfe7b084c707
Ben Ayed Mehdi <mehdibenayed97@gmail.com>
2022-11-09 06:51:53 +0100

    get companies

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,11 +1,43 @@
 import axios from 'axios'
 
 export default class apiCalls{
+    
+    static async GetAllPackages(){
+        const res = await axios.get('https://findyourwayapi.azurewebsites.net/api/Packages')
+        .catch(e=> console.log(e))
+        return res.data
+    }
+
+    static async GetAllFields(){
+        const res = await axios.get('https://findyourwayapi.azurewebsites.net/api/Fields')
+        .catch(e=> console.log(e))
+        return res.data
+    }
+
+    static async GetAllCategories(){
+        const res = await axios.get('https://findyourwayapi.azurewebsites.net/api/Categories')
+        .catch(e=> console.log(e))
+        return res.data
+    }
+
     static async GetAllUsers(){
         const res = await axios.get('https://findyourwayapi.azurewebsites.net/api/Users')
         .catch(e=> console.log(e))
         return res.data
     }
+
+    static async GetAllCompanies(){
+        const res = await axios.get('https://findyourwayapi.azurewebsites.net/api/Companies')
+        .catch(e=> console.log(e))
+        return res.data
+    }
+
+    static async GetAllProducts(){
+        const res = await axios.get('https://findyourwayapi.azurewebsites.net/api/Products')
+        .catch(e=> console.log(e))
+        return res.data
+    }
+    
 
     static async GetCompanyMilestones(id){
         const res = await axios.get(`https://findyourwayapi.azurewebsites.net/api/Milestones/${id}`, id)
--- src/pages/CompaniesPage.js
+++ src/pages/CompaniesPage.js
@@ -1,7 +1,9 @@
+import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 // @mui
 import { Grid, Button, Container, Stack, Typography } from '@mui/material';
 // components
+import apiCalls from '../api/apiCalls';
 import Iconify from '../components/iconify';
 import { BlogPostCard, BlogPostsSort, BlogPostsSearch } from '../sections/@dashboard/blog';
 // mock
@@ -18,6 +20,30 @@ const SORT_OPTIONS = [
 // ----------------------------------------------------------------------
 
 export default function CompaniesPage() {
+
+  const [companies, setCompanies] = useState([]);
+  const [user, setUser] = useState(() => {
+    const saved = localStorage.getItem('user');
+    const initialValue = JSON.parse(saved);
+    return initialValue || '';
+  });
+
+  useEffect(() => {
+    const getCompanies = async () => {
+      const companiesFromServer = await apiCalls.GetAllCompanies();
+      setCompanies(companiesFromServer);
+      // console.log('companies', companiesFromServer);
+    };
+    getCompanies();
+
+  }, []);
+
+  const allCompanies = companies.map((c, index) => {
+    const tmp = POSTS[index];
+    tmp.title = c.compnayName;
+    return tmp;
+  })
+
   return (
     <>
       <Helmet>
@@ -32,12 +58,12 @@ export default function CompaniesPage() {
         </Stack>
 
         <Stack mb={5} direction="row" alignItems="center" justifyContent="space-between">
-          <BlogPostsSearch posts={POSTS} />
+          <BlogPostsSearch posts={allCompanies} />
           <BlogPostsSort options={SORT_OPTIONS} />
         </Stack>
 
         <Grid container spacing={3}>
-          {POSTS.map((post, index) => (
+          {allCompanies.map((post, index) => (
             <BlogPostCard key={post.id} post={post} index={index} />
           ))}
         </Grid>
